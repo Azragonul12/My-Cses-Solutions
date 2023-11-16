9c6c30b3f7950ca16255e9860dc1c6c39c0596c9
Azragonul12 <145117610+Azragonul12@users.noreply.github.com>
2023-11-16 19:17:10 +0300

    Add files via upload

---
+++ BitStrings.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+#include<map>
+#include<string>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<utility>
+#include<set>
+#include<bitset>
+using namespace std;
+#define mod 1000000007
+long long Fastpow(long long Num, long long Pow){
+	if(Pow == 0){
+		return 1;
+	}
+	if(Pow % 2 == 1){
+		return (Num * (Fastpow(Num , Pow/2) % mod) * (Fastpow(Num, Pow/2) % mod)) % mod;
+	}
+	else{
+		return ((Fastpow(Num , Pow/2) % mod) * (Fastpow(Num, Pow/2) % mod)) % mod;
+	}
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	long long n;
+	cin>>n;
+	cout<<Fastpow(2,n) % mod;
+}
