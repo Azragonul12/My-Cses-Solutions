fe6c6c8718cac7d2701bd280fc2c766d31ca71db
Azragonul12 <145117610+Azragonul12@users.noreply.github.com>
2023-11-16 19:17:52 +0300

    Add files via upload

---
+++ CoinPiles.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+#include<map>
+#include<string>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<utility>
+#include<set>
+#include<bitset>
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	long long tst, first ,second;
+	cin>>tst;
+	while(tst--){
+		cin>>first>>second;
+		if((first + second) % 3 == 0 && ((first + 1) / 2 <= second) && second <= (first * 2)){
+			cout<<"YES\n";
+		}
+		else{
+			cout<<"NO\n";
+		}
+	}
+	
+
+}
