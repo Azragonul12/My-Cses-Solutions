d2feeeb6c42c73e84b3f082d6703abb642341243
Azragonul12 <145117610+Azragonul12@users.noreply.github.com>
2023-11-16 19:17:24 +0300

    Add files via upload

---
+++ TrailingZeros.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+#include<map>
+#include<string>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<utility>
+#include<set>
+#include<bitset>
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	long long n,count;
+	cin>>n;
+	count = 0;
+	while(n){
+		count += (n / 5);
+		n/=5;
+	}
+	cout<<count;
+
+}
