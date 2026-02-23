9e6833d005360866be7e70eb670e1f4e02121c3a
Azra Gönül Gürsoy <145117610+Azragonul12@users.noreply.github.com>
2026-02-23 21:37:36 +0300

    Update DigitQueries.cpp

--- DigitQueries.cpp
+++ DigitQueries.cpp
@@ -1,16 +1,4 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<math.h>
-#include<map>
-#include<string>
-#include<queue>
-#include<stack>
-#include<numeric>
-#include<utility>
-#include<set>
-#include<array>
-#include<bitset>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
 	ios_base::sync_with_stdio(false);
